9eda8fba2b9538e5c1c34158d5a6020a6fcd4de7
klu13 <klu13@ad.unc.edu>
2021-10-12 12:51:41 -0400

    connect backend to frontend and setup proxy

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,49 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    data: null
+  }
+
+  componentDidMount() {
+    this.backendTest()
+      .then(res => this.setState({ data: res.express }))
+      .catch(err => console.log(err));
+  }
+
+  backendTest = async () => {
+    const res = await fetch('/express_backend');
+    const body = await res.json();
+
+    if (res.status !== 200) {
+      throw Error(body.message)
+    }
+
+    return body;
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            {this.state.data}
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
