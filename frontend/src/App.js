d8fb0dca2165df0ae0ba0576791c18fb4e9aa5df
JacksonLei123 <jylei@live.unc.edu>
2021-10-14 13:30:28 -0400

    login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import LoginPage from './LoginPage';
 
 class App extends Component {
   state = {
@@ -26,22 +27,9 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            {this.state.data}
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+
+      <LoginPage/>
+      
     );
   }
 }
