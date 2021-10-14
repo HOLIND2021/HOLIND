563e09bf02ec5705906685c67394e32453cd3ead
Long Le <lelong@live.unc.edu>
2021-10-14 15:15:51 -0400

    Created navbar

---
+++ frontend/src/pages/Messages.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class Messages extends Component {
+    state = {
+      data: null
+    }
+  
+    componentDidMount() {
+      // what to do if component was mounted??
+    }
+  
+  
+    render() {
+      return (
+  
+        <div className='messages'>
+            <h1>Messagesajsidjaoisj</h1>
+        </div>
+        
+      );
+    }
+  }
+  
+  export default Messages;
