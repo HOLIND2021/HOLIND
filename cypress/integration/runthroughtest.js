c667ac96fb7eb0ce1256b772a79f69656d76976d
Jackson_Leigit config --global user.email jylei@ad.unc.edugit config --global pull.ff onlygit add .git commit -m testing$ git push origin maingit push origin branch-namegit config --global user.name Jackson_Lei <jylei@ad.unc.edu>
2021-11-07 13:03:19 -0500

    testing

---
+++ cypress/integration/runthroughtest.js
@@ -0,0 +1,109 @@
+
+
+
+let x = false;
+
+  describe('page loads', () => {
+    it('successfully loads', () => {
+      cy.visit('http://localhost:3000/') // change URL to match your dev URL
+  
+
+    })
+  })
+
+  describe('signup', () => {
+    it('finds input', function () {
+        cy.visit('http://localhost:3000/signup')
+        cy.get('#firstName')
+        .should('be.visible')
+        .type('joe')
+
+        cy.get('#lastName')
+        .should('be.visible')
+        .type('zhang')
+
+        cy.get('#email')
+        .should('be.visible')
+        .type('joezhang@gmail.com')
+
+        cy.get('#password')
+        .should('be.visible')
+        .type('12345678{enter}')
+
+        x = cy.includes('The email address is already in use by another account. (auth/email-already-in-use).')
+        cy.log(x)
+      })
+
+
+
+    })
+    
+    if (false) {
+
+      describe('logout', () => {
+        it('finds inputs and types in information', function () {
+            // destructuring assignment of the this.currentUser object
+            cy.get('#basic-button').click()
+            cy.contains('Logout').click()
+          })
+      })
+  
+    
+  
+    describe('login', () => {
+      it('finds inputs and types in information', function () {
+          // destructuring assignment of the this.currentUser object
+          cy.get('#email')
+          .should('be.visible')
+          .type('bigbobho@gmail.com')
+  
+          cy.get('#password')
+          .should('be.visible')
+          .type('12345678{enter}')
+        })
+    })
+  
+    describe('navigate home page', () => {
+      it('navigate through patient treatments', function () {
+          // destructuring assignment of the this.currentUser object
+          cy.contains('Upcoming').click()
+          cy.contains('Overdue').click()
+          cy.contains('Completed').click()
+  
+          cy.get('.home').get('h4')
+            .contains('Bob')
+            .contains('Ho')
+            
+  
+          
+        })
+    })
+  
+    describe('navigate menu items', () => {
+      it('navigate through patient treatments', function () {
+          // destructuring assignment of the this.currentUser object
+          cy.get('nav').get('ul').click({force: true})
+          cy.get('nav').contains('Patients').click()
+          cy.get('nav').get('ul').click({force: true})
+          cy.get('nav').contains('Messages').click()
+          cy.get('nav').get('ul').click({force: true})
+          cy.get('nav').contains('Help').click()
+  
+          
+        })
+    })
+  
+    describe('logout', () => {
+      it('finds inputs and types in information', function () {
+          // destructuring assignment of the this.currentUser object
+          cy.get('#basic-button').click()
+          cy.contains('Logout').click()
+        })
+    })
+
+
+
+    }
+
+    
+
