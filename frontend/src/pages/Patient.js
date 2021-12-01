3409a773a5a6d40a2f6e3da27f96cef8bd876f84
klu13 <klu13@ad.unc.edu>
2021-12-01 01:14:11 -0500

    load patient page when user is signed in

--- frontend/src/pages/Patient.js
+++ frontend/src/pages/Patient.js
@@ -35,17 +35,17 @@ class Patient extends Component {
     async componentDidMount() {
         firebaseAuth.onAuthStateChanged(async (user) => {
             if (user) {
-              // User is signed in, see docs for a list of available properties
-              // https://firebase.google.com/docs/reference/js/firebase.User
-              const uid = user.uid;
-              const res = await fetch(`${process.env.REACT_APP_API}/api/user/${uid}`);
-              const body = await res.json();
-              if (res.status === 200) {
-                this.setState({ user: body.body });
-              }
+                // User is signed in, see docs for a list of available properties
+                // https://firebase.google.com/docs/reference/js/firebase.User
+                const uid = user.uid;
+                const res = await fetch(`${process.env.REACT_APP_API}/api/user/${uid}`);
+                const body = await res.json();
+                if (res.status === 200) {
+                    this.setState({ user: body.body });
+                }
             } else {
-              // User is signed out
-              // ...
+                // User is signed out
+                // ...
             }
         });
         this.setState({
@@ -173,111 +173,113 @@ class Patient extends Component {
         };
 
         return (
-            <div className='patient'>
-                <Typography variant="h4" sx={{ fontWeight: "bold" }}>
-                    {this.state.first} {this.state.last}
-                </Typography>
-                <Tooltip title="Upload Video">
-                    <IconButton aria-label="uploadVideo" sx={{ padding: '15px', marginTop: '10px' }}>
-                        <VideoCameraBackIcon color="action" fontSize="large"></VideoCameraBackIcon>
-                    </IconButton>
-                </Tooltip>
-                <Tooltip title="Direct Message">
-                    <IconButton aria-label="message" sx={{ padding: '15px', marginTop: '10px', marginLeft: '15px' }}>
-                        <EmailRoundedIcon color="action" fontSize="large"></EmailRoundedIcon>
-                    </IconButton>
-                </Tooltip>
-                {this.state.user && this.state.user.role !== 'patient' ? <Tooltip title="Patient Analytics">
-                    <IconButton aria-label="message" sx={{ padding: '15px', marginTop: '10px', marginLeft: '15px' }}>
-                        <AssessmentRoundedIcon color="action" fontSize="large"></AssessmentRoundedIcon>
-                    </IconButton>
-                </Tooltip> : ''}
-                <List
-                    sx={{ width: '100%', maxWidth: 480, bgcolor: 'background.paper' }}
-                    component="nav"
-                    aria-labelledby="nested-list-subheader"
-                >
-                    {taskList.map((task, index) => (
-                        <div>
-                            <ListItemButton onClick={() => handleOpen(index)}>
-                                <ListItemText primaryTypographyProps={{ style: category_text }} primary={task.name} />
-                                {this.state.open[index] ? <ExpandLess /> : <ExpandMore />}
-                            </ListItemButton>
-                            <Collapse in={this.state.open[index]} timeout="auto" unmountOnExit>
-                                <List component="div" disablePadding>
-                                    {this.state.exercises.sort((a,b) => (a.due > b.due) ? 1 : (b.due > a.due) ? -1 : 0).map((exercise) => { // sort exercises in ascending order by due date
-                                        let date = new Date(exercise.due);
-                                        let todaysDate = new Date();
-                                        let statusValue = "";
-                                        let tomorrowDate = new Date(todaysDate.getFullYear(), todaysDate.getMonth(), todaysDate.getDate() + 1);
-                                        let dayAfterTomorrowDate = new Date(todaysDate.getFullYear(), todaysDate.getMonth(), todaysDate.getDate() + 2);
-                                        let lastDayofWeekDate = new Date(todaysDate.getFullYear(), todaysDate.getMonth(), todaysDate.getDate() + 6);
-                                        if (exercise.status === "completed") {
-                                            statusValue = "completed";
-                                        // check if due date is today
-                                        } else if (date.setHours(0,0,0,0) === todaysDate.setHours(0,0,0,0)) {
-                                            statusValue = "today";
-                                        // check if date is tomorrow
-                                        } else if (date.setHours(0,0,0,0) === tomorrowDate.setHours(0,0,0,0)) {
-                                            statusValue = "tomorrow";
-                                        // check if date is this week
-                                        } else if (date >= dayAfterTomorrowDate && date <= lastDayofWeekDate) {
-                                            statusValue = "this_week";
-                                        // check if date is after this week
-                                        } else if (date >= lastDayofWeekDate) {
-                                            statusValue = "later";
-                                        // check if date is before today
-                                        } else if (date < todaysDate) {
-                                            statusValue = "overdue";
-                                        }
+            <div>
+                {this.state.user ? <div className='patient'>
+                    <Typography variant="h4" sx={{ fontWeight: "bold" }}>
+                        {this.state.first} {this.state.last}
+                    </Typography>
+                    <Tooltip title="Upload Video">
+                        <IconButton aria-label="uploadVideo" sx={{ padding: '15px', marginTop: '10px' }}>
+                            <VideoCameraBackIcon color="action" fontSize="large"></VideoCameraBackIcon>
+                        </IconButton>
+                    </Tooltip>
+                    <Tooltip title="Direct Message">
+                        <IconButton aria-label="message" sx={{ padding: '15px', marginTop: '10px', marginLeft: '15px' }}>
+                            <EmailRoundedIcon color="action" fontSize="large"></EmailRoundedIcon>
+                        </IconButton>
+                    </Tooltip>
+                    {this.state.user.role !== 'patient' ? <Tooltip title="Patient Analytics">
+                        <IconButton aria-label="message" sx={{ padding: '15px', marginTop: '10px', marginLeft: '15px' }}>
+                            <AssessmentRoundedIcon color="action" fontSize="large"></AssessmentRoundedIcon>
+                        </IconButton>
+                    </Tooltip> : ''}
+                    <List
+                        sx={{ width: '100%', maxWidth: 480, bgcolor: 'background.paper' }}
+                        component="nav"
+                        aria-labelledby="nested-list-subheader"
+                    >
+                        {taskList.map((task, index) => (
+                            <div>
+                                <ListItemButton onClick={() => handleOpen(index)}>
+                                    <ListItemText primaryTypographyProps={{ style: category_text }} primary={task.name} />
+                                    {this.state.open[index] ? <ExpandLess /> : <ExpandMore />}
+                                </ListItemButton>
+                                <Collapse in={this.state.open[index]} timeout="auto" unmountOnExit>
+                                    <List component="div" disablePadding>
+                                        {this.state.exercises.sort((a, b) => (a.due > b.due) ? 1 : (b.due > a.due) ? -1 : 0).map((exercise) => { // sort exercises in ascending order by due date
+                                            let date = new Date(exercise.due);
+                                            let todaysDate = new Date();
+                                            let statusValue = "";
+                                            let tomorrowDate = new Date(todaysDate.getFullYear(), todaysDate.getMonth(), todaysDate.getDate() + 1);
+                                            let dayAfterTomorrowDate = new Date(todaysDate.getFullYear(), todaysDate.getMonth(), todaysDate.getDate() + 2);
+                                            let lastDayofWeekDate = new Date(todaysDate.getFullYear(), todaysDate.getMonth(), todaysDate.getDate() + 6);
+                                            if (exercise.status === "completed") {
+                                                statusValue = "completed";
+                                                // check if due date is today
+                                            } else if (date.setHours(0, 0, 0, 0) === todaysDate.setHours(0, 0, 0, 0)) {
+                                                statusValue = "today";
+                                                // check if date is tomorrow
+                                            } else if (date.setHours(0, 0, 0, 0) === tomorrowDate.setHours(0, 0, 0, 0)) {
+                                                statusValue = "tomorrow";
+                                                // check if date is this week
+                                            } else if (date >= dayAfterTomorrowDate && date <= lastDayofWeekDate) {
+                                                statusValue = "this_week";
+                                                // check if date is after this week
+                                            } else if (date >= lastDayofWeekDate) {
+                                                statusValue = "later";
+                                                // check if date is before today
+                                            } else if (date < todaysDate) {
+                                                statusValue = "overdue";
+                                            }
 
-                                        if (statusValue === task.value) {
-                                            return <ListItemButton sx={{ pl: 4 }}>
-                                                <ListItemText primary={exercise.name} primaryTypographyProps={{
-                                                    style: {
-                                                        whiteSpace: 'nowrap',
-                                                        overflow: 'hidden',
-                                                        textOverflow: 'ellipsis', 
-                                                        maxWidth: "220px",
-                                                        paddingRight: "10px"
-                                                    }
-                                                }} />
-                                                <CalendarTodayRoundedIcon fontSize="small"></CalendarTodayRoundedIcon>
-                                                <Typography sx={{paddingLeft: "10px", paddingRight: "10px"}}>{new Date(exercise.due).toLocaleString('en-En',{weekday: "short", month: "short", day: "numeric"})}</Typography>
-                                                <Checkbox checked={exercise.status === "completed"} name={exercise.name} id={exercise.due} onChange={handleComplete} />
-                                                {this.state.user && this.state.user.role !== 'patient' ? <ExerciseOptionsMenu exercise={exercise} state={this.state} updateExercises={this.updateExercises} /> : ''}
-                                            </ListItemButton>
-                                        } else return '';
-                                    })}
-                                </List>
-                            </Collapse>
-                        </div>
-                    ))}
-                </List>
+                                            if (statusValue === task.value) {
+                                                return <ListItemButton sx={{ pl: 4 }}>
+                                                    <ListItemText primary={exercise.name} primaryTypographyProps={{
+                                                        style: {
+                                                            whiteSpace: 'nowrap',
+                                                            overflow: 'hidden',
+                                                            textOverflow: 'ellipsis',
+                                                            maxWidth: "220px",
+                                                            paddingRight: "10px"
+                                                        }
+                                                    }} />
+                                                    <CalendarTodayRoundedIcon fontSize="small"></CalendarTodayRoundedIcon>
+                                                    <Typography sx={{ paddingLeft: "10px", paddingRight: "10px" }}>{new Date(exercise.due).toLocaleString('en-En', { weekday: "short", month: "short", day: "numeric" })}</Typography>
+                                                    <Checkbox checked={exercise.status === "completed"} name={exercise.name} id={exercise.due} onChange={handleComplete} />
+                                                    {this.state.user.role !== 'patient' ? <ExerciseOptionsMenu exercise={exercise} state={this.state} updateExercises={this.updateExercises} /> : ''}
+                                                </ListItemButton>
+                                            } else return '';
+                                        })}
+                                    </List>
+                                </Collapse>
+                            </div>
+                        ))}
+                    </List>
 
-                {this.state.user && this.state.user.role !== 'patient' ? <Button variant="outlined" sx={{ marginTop: '20px' }} onClick={() => this.setState({ showDialog: !this.state.showDialog })}>
-                    Add Task
-                </Button> : ''}
-                <Dialog open={this.state.showDialog} component="form" onSubmit={handleSubmit}>
-                    <DialogTitle>Add Task</DialogTitle>
-                    <DialogContent>
-                        <TextField
-                            margin="normal"
-                            id="title"
-                            label="Task Name"
-                            name="title"
-                            autoFocus
-                            required
-                        />
-                        <br></br>
-                        <br></br>
-                        <CalendarDate />
-                    </DialogContent>
-                    <DialogActions>
-                        <Button onClick={() => this.setState({ showDialog: !this.state.showDialog })}>Cancel</Button>
-                        <Button type="submit" variant="contained">Add Task</Button>
-                    </DialogActions>
-                </Dialog>
+                    {this.state.user.role !== 'patient' ? <Button variant="outlined" sx={{ marginTop: '20px' }} onClick={() => this.setState({ showDialog: !this.state.showDialog })}>
+                        Add Task
+                    </Button> : ''}
+                    <Dialog open={this.state.showDialog} component="form" onSubmit={handleSubmit}>
+                        <DialogTitle>Add Task</DialogTitle>
+                        <DialogContent>
+                            <TextField
+                                margin="normal"
+                                id="title"
+                                label="Task Name"
+                                name="title"
+                                autoFocus
+                                required
+                            />
+                            <br></br>
+                            <br></br>
+                            <CalendarDate />
+                        </DialogContent>
+                        <DialogActions>
+                            <Button onClick={() => this.setState({ showDialog: !this.state.showDialog })}>Cancel</Button>
+                            <Button type="submit" variant="contained">Add Task</Button>
+                        </DialogActions>
+                    </Dialog>
+                </div> : ''}
             </div>
         );
     }
