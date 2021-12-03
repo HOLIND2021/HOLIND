e08fd5129f51b5df070c951b731594d05a67c862
Jackson_Leigit config --global user.email jylei@ad.unc.edugit config --global pull.ff onlygit add .git commit -m testing$ git push origin maingit push origin branch-namegit config --global user.name Jackson_Lei <jylei@ad.unc.edu>
2021-12-03 15:17:01 -0500

    commit

--- frontend/src/pages/Messages.js
+++ frontend/src/pages/Messages.js
@@ -1,24 +1,107 @@
 import React, { Component } from 'react';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import Divider from '@mui/material/Divider';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
+import Typography from '@mui/material/Typography';
 
 class Messages extends Component {
     state = {
-      data: null
+      contactDisplayed: null
     }
   
     componentDidMount() {
       // what to do if component was mounted??
     }
+
+    MessagesList() {
+
+      let message = <ListItemButton alignItems="flex-start" onClick={() => {
+        
+        this.setState({contactDisplayed: "hi"});
+            }}>
+            
+            <ListItemText
+              primary="Hello there"
+              secondary={
+                <React.Fragment>
+                  <Typography
+                    sx={{ display: 'inline' }}
+                    component="span"
+                    variant="body2"
+                    color="text.primary"
+                  >
+                    Kevin Lu
+                  </Typography>
+                  {" — Hi how are you"}
+                </React.Fragment>
+              }
+            />
+            <Divider variant="inset" component="li" />
+          </ListItemButton>
+      let message2 = <ListItemButton alignItems="flex-start" onClick={() => {
+        
+        this.setState({contactDisplayed: "adsfasd"});
+            }}>
+            
+            <ListItemText
+              primary="Hello there"
+              secondary={
+                <React.Fragment>
+                  <Typography
+                    sx={{ display: 'inline' }}
+                    component="span"
+                    variant="body2"
+                    color="text.primary"
+                  >
+                    Jackson Lei
+                  </Typography>
+                  {" — Finish this task please"}
+                </React.Fragment>
+              }
+            />
+            <Divider variant="inset" component="li" />
+          </ListItemButton>
+      let messages = [message, message2]
+
+      return messages;
+
+    }
   
   
     render() {
+      
       return (
+        <table>
+          <tr>
+            <td>
+
+              <React.Fragment>
+              <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+
+                  {this.MessagesList()}
+      
+              </List>
+    
+              </React.Fragment>
+
+            </td>
+
+            <td>
+              {this.state.contactDisplayed}
+            
+            </td>
+          </tr>
+        </table>
   
-        <div className='messages'>
-            <h1>Messages</h1>
-        </div>
-        
       );
     }
   }
   
   export default Messages;
+  
+
+
